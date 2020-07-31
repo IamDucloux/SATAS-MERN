329050ed576d8943db4e92867e69685945a78827
IamDucloux <ianducloux@gmail.com>
2020-07-30 23:51:12 -0500

    Basic config

---
+++ backend/src/controllers/models/routes/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// SETTINGS
+app.set('port', process.env.PORT || 4000);
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+app.use('/', () => {console.log('Hello world')});
+/* 
+app.use('/api/login', require('./routes/login'));
+app.use('/api/register', require('./routes/register'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/dashboard', require('./routes/dashboard')); 
+*/
+
+
+module.exports = app;
