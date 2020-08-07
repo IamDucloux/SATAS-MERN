474281ccc3a494ae626f3815aac1e083b8e2cbe6
IamDucloux <ianducloux@gmail.com>
2020-08-06 22:00:29 -0500

    Info component was added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Navigation from './components/navigation'
+import Info from './components/info'
   
 function App() {
   return (
     <Router>
       <Navigation />
+      <Info />
     </Router>
+    
   );
 }
 
---
+++ frontend/src/components/info.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class info extends Component {
+    render() {
+        return (
+            <div className='info-container'>
+                <h2>Pre-Alpha 1.0</h2>
+                <p>
+                    <strong>WELCOME</strong><br/>
+                    This is an early version of the app, and was developed under the objetive of learn the
+                    <strong>MERN 🔥</strong> stack.
+
+                </p>
+            </div>
+        )
+    }
+}
