6dced0c67ed80f276bcc0d8ad1996d7c1372ea55
IamDucloux <ianducloux@gmail.com>
2020-08-13 12:42:56 -0500

    Dashboard and Orders components added

---
+++ frontend/src/components/createOrder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class createOrder extends Component {
+    render() {
+        return (
+            <div className="createOrder-container">
+                
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class dashboard extends Component {
+
+    
+
+    render() {
+        return (
+            <div className="dashboard-container">
+                
+            </div>
+        )
+    }
+}
