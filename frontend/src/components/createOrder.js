537352b971ebde147a63ac7bad942f0d84985700
IamDucloux <ianducloux@gmail.com>
2020-08-14 14:08:16 -0500

    Added the basic structure to the Dashboard and the Orders

--- frontend/src/components/createOrder.js
+++ frontend/src/components/createOrder.js
@@ -5,7 +5,26 @@ export default class createOrder extends Component {
     render() {
         return (
             <div className="createOrder-container">
-                
+                <div className="create-order">
+                    <h2>CREATE ORDER</h2>
+                    <div className="c-order">
+                        <form>
+                            <label>campo1</label>
+                            <input type="text"></input>
+                            <label>campo1</label>
+                            <input type="text"></input>
+                            <label>campo1</label>
+                            <input type="text"></input>
+                        </form>
+                    </div>
+
+                </div>
+                <div className="show-orders">
+                    <h2>ORDERS</h2>
+                    <div className="orders">
+
+                    </div>
+                </div>
             </div>
         )
     }
--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -2,12 +2,35 @@ import React, { Component } from 'react'
 
 export default class dashboard extends Component {
 
-    
+
 
     render() {
         return (
             <div className="dashboard-container">
-                
+                <div className="chart-container" id="c1">
+                    <h2>VENTAS</h2>
+                    <div className="data-container">
+                        <div className="data">
+                            <h3>$250,000.00</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="chart-container" id="c2">
+                    <h2>COSTOS</h2>
+                    <div className="data-container">
+                        <div className="data">
+                        <h3>$125,000.00</h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="chart-container" id="c3">
+                    <h2>GRAFICA</h2>
+                    <div className="data-container">
+                        <div className="data">
+                            <div className="loading"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
